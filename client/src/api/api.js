37378eb2bbd47eca3f6a7fc33173bb68ebfa9a2d
Max Henrique <maxhb.df@gmail.com>
2020-12-14 02:05:42 -0300

    update main and api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const url =
   process.env.NODE_ENV === 'production'
     ? 'http://backend-gces-max.herokuapp.com'
-    : 'http://localhost:3000';
+    : 'http://localhost:3000'
 
-const base = `${url}/api/v1`;
+const base = `${url}/api/v1`
 const API = axios.create({
   baseURL: base,
-});
+})
 
 export default API;
