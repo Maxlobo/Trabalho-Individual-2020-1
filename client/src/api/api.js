7c6ed57dabc0e2fcd43993c460ad862707086a1b
Max Henrique <maxhb.df@gmail.com>
2020-12-14 01:03:40 -0300

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const url =
-  process.env.NODE_ENV === "production"
-    ? "http://backend-gces-max.herokuapp.com"
-    : "http://localhost:3000";
+  process.env.NODE_ENV === 'production'
+    ? 'http://backend-gces-max.herokuapp.com'
+    : 'http://localhost:3000';
 
 const base = `${url}/api/v1`;
 const API = axios.create({
